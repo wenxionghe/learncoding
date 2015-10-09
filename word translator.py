740375dd89be0a0b825b63582c784b603776f1f5
wenxionghe <zxp6125@gmail.com>
2015-10-08 23:00:33 -0400

    My first Python homework
    This is my first program. Basically it removes the first letter of the
    word and add "ay" at the end of it

---
+++ word translator.py
@@ -0,0 +1,13 @@
+# This is my first program. Basically it removes the first letter of the word and add "ay" at the end of it.
+pyg = 'ay'
+
+original = raw_input('Enter a word:')
+
+if len(original) > 0 and original.isalpha():
+    word = original.lower()
+    first = word[0]
+    answer = word[1:len(original)]
+    new_word = answer + first + pyg
+    print new_word
+else:
+    print 'Empty! You didn''t type in any word son!'
